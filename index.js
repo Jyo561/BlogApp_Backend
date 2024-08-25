463b2c8f2fd0c65c37150563e8ead60b9c5eb34a
Jyo561 <76492740+Jyo561@users.noreply.github.com>
2024-08-25 19:58:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ const sequelize = require('./config/database');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://blog-frontend-eta-eight.vercel.app',
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true,
-}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', postRoutes);
 
