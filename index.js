bff8ee569505296be6709cc52208dc090e222d42
Jyo561 <76492740+Jyo561@users.noreply.github.com>
2024-08-25 20:12:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const options = [
     credentials: true,
   })
 ];
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(options);
 app.use(bodyParser.json());
 app.use('/api', postRoutes);
