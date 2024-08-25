8dce5c89eb34c652bb9b5f1ac87e3d1ddaf37241
Jyo561 <76492740+Jyo561@users.noreply.github.com>
2024-08-25 20:42:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,7 @@ const sequelize = require('./config/database');
 require('dotenv').config();
 
 const app = express();
-const options = [
-  cors({
-    origin: '*',
-    methods: '*',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  })
-];
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
-app.use(options);
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', postRoutes);
 
